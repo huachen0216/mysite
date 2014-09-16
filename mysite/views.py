3ba02797a87c2798c1a4dc427a2dbcebee40b106
HUACHEN <huachen0216@gmail.com>
2014-09-16 16:51:15 +0800

    lenovo 0916 second commit

--- mysite/views.py
+++ mysite/views.py
@@ -1,16 +1,20 @@
-from django.template.loader import get_template
-from django.template import Context
-from django.http import Http404, HttpResponse
+#from django.template.loader import get_template
+#from django.template import Context
+from django.shortcuts import render_to_response
+from django.http import Http404
 import datetime
 
 def hello(request):
     return HttpResponse("Hello world")
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+    #now = datetime.datetime.now()
+    #return render_to_response('current_datetime.html', {'current_date': now})
+    #t = get_template('current_datetime.html')
+    #html = t.render(Context({'current_date': now}))
+    #return HttpResponse(html)
+    current_date = datetime.datetime.now()
+    return render_to_response('dateapp/current_datetime.html', locals())
 
 def hours_ahead(request, offset):
     try:
