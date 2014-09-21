ee844740c31c71db8209a8a695b789f34ea4145b
HUACHEN <huachen0216@gmail.com>
2014-09-21 22:47:26 +0800

    last commit on 20140921 with thinkpad.

--- books/models.py
+++ books/models.py
@@ -17,16 +17,16 @@ class Publisher(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    email = models.EmailField(blank=True)
+    email = models.EmailField(blank=True, verbose_name='e-mail')
     
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True, null=True)
+    authors = models.ManyToManyField(Author)
     
     def __unicode__(self):
         return self.title
