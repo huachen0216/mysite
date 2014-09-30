a8187857005ee3c61f098371172a83d8f26f6c3a
HUACHEN <huachen0216@gmail.com>
2014-09-30 23:44:44 +0800

    django_form_first_commit

--- books/views.py
+++ books/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# from django.shortcuts import render_to_reponse
 
-# Create your views here.
+from django.shortcuts import render_to_response
+
+def search_form(request):
+    return render_to_response('search_form.html')
